2a73b63b73217ccbf17d809d2acc244503aa9168
maxamarcus <mmarcus11@fordham.edu>
2025-07-31 11:41:50 -0400

    main program written

---
+++ cmd/todo/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"todo"
+)
+
+const todoFileName = ".todo.json"
+
+func main() {
+	l := &todo.List{}
+
+	if err := l.Get(todoFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	switch {
+
+    // No args -- print list
+	case len(os.Args) == 1:
+		for _, item := range *l {
+			fmt.Println(item.Task)
+		}
+
+    // Concatenate args and add to list.
+	default:
+		item := strings.Join(os.Args[1:], " ")
+		l.Add(item)
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Println(os.Stderr, err)
+			os.Exit(1)
+		}
+	}
+}
